f8110df98c6377e2b43c72b1197046dc5495f480
Thong LT <letrthong@gmail.com>
2022-10-30 11:15:04 +0100

    curl

---
+++ curl/build.sh
@@ -0,0 +1,1 @@
+g++  hello.cpp  -std=c++11  -lpthread -lcurl -I /usr/local/include/  
---
+++ curl/run.sh
@@ -0,0 +1,33 @@
+# git clone https://github.com/curl/curl.git
+cd curl
+mkdir build
+cd build
+cmake ..
+make  
+sudo make install
+
+exit 
+Install configuration: ""
+-- Installing: /usr/local/lib/libcurl.so
+-- Installing: /usr/local/bin/curl
+-- Set runtime path of "/usr/local/bin/curl" to ""
+-- Installing: /usr/local/bin/curl-config
+-- Installing: /usr/local/lib/pkgconfig/libcurl.pc
+-- Installing: /usr/local/include/curl
+-- Installing: /usr/local/include/curl/urlapi.h
+-- Installing: /usr/local/include/curl/curlver.h
+-- Installing: /usr/local/include/curl/easy.h
+-- Installing: /usr/local/include/curl/curl.h
+-- Installing: /usr/local/include/curl/typecheck-gcc.h
+-- Installing: /usr/local/include/curl/header.h
+-- Installing: /usr/local/include/curl/options.h
+-- Installing: /usr/local/include/curl/mprintf.h
+-- Installing: /usr/local/include/curl/system.h
+-- Installing: /usr/local/include/curl/multi.h
+-- Installing: /usr/local/include/curl/stdcheaders.h
+-- Installing: /usr/local/include/curl/websockets.h
+-- Installing: /usr/local/lib/cmake/CURL/CURLTargets.cmake
+-- Installing: /usr/local/lib/cmake/CURL/CURLTargets-noconfig.cmake
+-- Installing: /usr/local/lib/cmake/CURL/CURLConfigVersion.cmake
+-- Installing: /usr/local/lib/cmake/CURL/CURLConfig.cmake
+
