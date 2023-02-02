7a89ac1888a4601c62dd0881fda593306fbd32fb
Thong LT <letrthong@gmail.com>
2023-02-02 08:07:05 +0100

    curl post

--- curl/build.sh
+++ curl/build.sh
@@ -1,6 +1,6 @@
-g++  get.cpp  -std=c++11  -lpthread -lcurl -I /usr/local/include/  
+#g++  get.cpp  -std=c++11  -lpthread -lcurl -I /usr/local/include/  
+#./a.out
+
+
+g++  post.cpp  -std=c++11  -lpthread -lcurl -I /usr/local/include/  
 ./a.out
-
-
-# g++  post.cpp  -std=c++11  -lpthread -lcurl -I /usr/local/include/  
-# ./a.out
